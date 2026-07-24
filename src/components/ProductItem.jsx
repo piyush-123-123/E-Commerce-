c8871bd51daa0733e905c13b212af7c0c605ab1c
Your Name <your-email@example.com>
2026-07-24 12:11:34 +0530

    Sending cart data using Thunk

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,10 +2,8 @@ import { Card, Button } from "react-bootstrap";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import {uiActions} from "../store/redux_store/uiSlice"
 import AuthContext from "../store/AuthContext";
-import { cartActions } from "../store/redux_store/cartSlice";
-import API_URL from "../API";
+import  {sendCartData} from "../store/redux_store/cart-actions";
 
 const ProductItem = ({ product }) => {
   const dispatch = useDispatch();
@@ -16,65 +14,8 @@ const ProductItem = ({ product }) => {
     : "";
 
   const addToCartHandler = async () => {
-    try {
-      dispatch(uiActions.showNotification({
-            status: "pending",
-            title: "Sending...",
-            message: "Sending cart data!"
-      }))
-      const response = await fetch(`${API_URL}/cart${userId}`);
-      const cartItems = await response.json();
-      const existingItem = cartItems.find(
-        (item) => item.id === product.id
-      );
-      if (existingItem) {
-        const { _id, ...updatedItem } = existingItem;
-        await fetch(`${API_URL}/cart${userId}/${_id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ...updatedItem,
-            quantity: existingItem.quantity + 1,
-          }),
-        });
-      } else {
-        await fetch(`${API_URL}/cart${userId}`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ...product,
-            quantity: 1,
-          }),
-        });
-      }
-      const updatedResponse = await fetch(`${API_URL}/cart${userId}`);
-      const updatedCart = await updatedResponse.json();
-      dispatch(cartActions.setItems(updatedCart));
-      console.log("Success dispatch");
-      dispatch(
-     uiActions.showNotification({
-       status: "success",
-        title: "Success!",
-       message: "Item added to cart successfully."
-      })
-      
-     );
-       setTimeout(() => {
-      dispatch(uiActions.hideNotification()); 
-    }, 2000);
-    } catch (err) {
-      dispatch(
-     uiActions.showNotification({
-       status: "error",
-    title: "Error!",
-    message: "Sending request failed."
-  })
-);
-    }
+    dispatch(sendCartData({ userId, product }));
+
   };
 
   return (
--- src/store/redux_store/cart-actions.js
+++ src/store/redux_store/cart-actions.js
@@ -1,6 +1,89 @@
 import API_URL from "../../API";
 import { cartActions } from "./cartSlice";
 import { uiActions } from "./uiSlice";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const sendCartData=createAsyncThunk(
+  "cart/sendCartData",
+  async ({userId,product},{dispatch,rejectWithValue})=>{
+    try{
+        dispatch (
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    )
+    const response = await fetch(`${API_URL}/cart${userId}`);
+    if(!response.ok){
+      throw new Error("Could not fetch Cart Data");
+    }
+    const cartItems= await response.json();
+    const existingItem=cartItems.find(
+      (item)=>item.id===product.id
+    );
+          if (existingItem) {
+        const { _id, ...updatedItem } = existingItem;
+        const putResponse=await fetch(`${API_URL}/cart${userId}/${_id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ...updatedItem,
+            quantity: existingItem.quantity + 1,
+          }),
+        })
+        if (!putResponse.ok) {
+        throw new Error("Could not update cart.");
+        }
+      } else {
+        await fetch(`${API_URL}/cart${userId}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ...product,
+            quantity: 1,
+          }),
+        });
+      }
+      const updatedResponse = await fetch(`${API_URL}/cart${userId}`);
+      if (!updatedResponse.ok) {
+      throw new Error("Could not fetch updated cart!");
+      }
+      const updatedCart = await updatedResponse.json();
+      dispatch(cartActions.setItems(updatedCart));
+      console.log("Success dispatch");
+      dispatch(
+     uiActions.showNotification({
+       status: "success",
+        title: "Success!",
+       message: "Item added to cart successfully."
+      })
+      
+     );
+       setTimeout(() => {
+      dispatch(uiActions.hideNotification()); 
+    }, 2000);
+    }
+
+    catch(err){
+          dispatch(
+          uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending request failed."
+         }))
+          setTimeout(() => {
+          dispatch(uiActions.hideNotification());
+          }, 2000);
+      return rejectWithValue(err.message);
+
+    }
+  }
+)
 
 export const fetchCartData = (userId) => {
   return async (dispatch) => {
